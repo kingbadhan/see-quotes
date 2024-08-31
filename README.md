0b69ebf1f8dd7a81f48669a54cb5aa7e39694e95
king_badhan <pankaj4utanda@gmail.com>
2024-08-31 22:04:05 +0530

    buildSetup

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# projects
-## created by coder.lp
+# saySo
---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+const path = require("path");
+
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.send("server working well");
+});
+
+app.listen(port, () => console.log("8080"));
